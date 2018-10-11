5a52990eb37b03c30f0f3ad6c455a91072cc3be3
Ruben <ruben.delafuente@hotmail.com>
2018-10-11 12:55:22 +0200

    Subir al repositorio la clase Contacto

--- src/Contacto.java
+++ src/Contacto.java
@@ -10,4 +10,29 @@
  */
 public class Contacto {
     
+    private String nombre;
+    private String telefono; 
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Contacto(String nombre, String telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+    
+     
 }
